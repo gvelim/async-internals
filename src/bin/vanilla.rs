b63846615dadef3c34c13d0e6c4a7be36debd6e7
gvelim <github@velimachitis.me>
2022-04-10 10:35:12 +0100

    vanilla.rs + thread-pool implementation

--- src/bin/vanilla.rs
+++ src/bin/vanilla.rs
@@ -1,7 +1,7 @@
 use rand::prelude::*;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 use futures::executor;
-use futures::executor::{LocalPool, ThreadPool};
+use futures::executor::{block_on};
 use futures::task::SpawnExt;
 use async_test::myfuture::{
     MyTimer,
@@ -37,16 +37,33 @@ fn run_localexec() {
         pool.spawner()
             .spawn(async move {
                 println!("F{}:{}", i, MyTimer::new(d).await);
-            });
+            }).unwrap();
     }
-
     pool.run();
+}
 
+fn run_threadpool_exec() {
+    let pool = executor::ThreadPool::new().expect("Error: cannot initiate pool");
+    let mut hnd = Vec::new();
+
+    for i in (1..=10).rev() {
+        let d: u64 = thread_rng().gen_range(1..=10);
+        let h = pool.spawn_with_handle(async move {
+                println!("F{}:{}", i, MyTimer::new(d).await);
+            }).unwrap();
+        hnd.push(h);
+    }
+    for h in hnd {
+        print!("{:?} = ", h);
+        println!("{:?}", block_on(h));
+    }
 }
 
 fn main() {
-    println!("Running futures using myexecutor");
+    println!("MyExecutor:");
     run_myexec();
-    println!("Running futures using futures::executor");
+    println!("LocalThread:");
     run_localexec();
+    println!("ThreadPool:");
+    run_threadpool_exec();
 }
